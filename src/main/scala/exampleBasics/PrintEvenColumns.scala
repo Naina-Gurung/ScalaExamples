ee4b9be3fa1a10f6b0801464820f184d9d1774ad
Naina-Gurung <nainagurung517@gmail.com>
2020-01-09 00:39:33 +0530

    print even columns using zipwithIndex

--- src/main/scala/exampleBasics/PrintEvenColumns.scala
+++ src/main/scala/exampleBasics/PrintEvenColumns.scala
@@ -1,8 +1,13 @@
 package exampleBasics
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object PrintEvenColumns {
+
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("org").setLevel(Level.OFF)
+
   def main(args: Array[String]): Unit = {
     val spark=  SparkSession.builder().master("local").getOrCreate()
 
@@ -12,6 +17,7 @@ object PrintEvenColumns {
 
     val parentColumns= empDf.columns.toList
 
+    print(parentColumns)
     val evenCols= parentColumns.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).toSeq
 
     val childDf= empDf.select(evenCols.head, evenCols.tail:_*).show()
